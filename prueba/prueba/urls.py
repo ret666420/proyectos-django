c3e47fff354cf9b1fdbafc4f3b0dfb32379bce10
Reynaldo Hernández Vega <reynaldohernadezvega@gmail.com>
2025-06-24 20:34:54 -0600

    trabajando con templates

--- prueba/prueba/urls.py
+++ prueba/prueba/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from inicio import views
 from django.conf import settings
+from registros import views as views_registros
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views_registros.registros, name="Principal"),
     path('',views.principal,name="Principal"),
     path('contacto/',views.contacto, name="Contacto"),
     path('formulario/', views.formulario, name="Formulario"),
